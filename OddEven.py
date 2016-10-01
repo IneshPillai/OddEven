e70d92da972410e0f9291b0a289ee9c70c342ce9
Pranay <dhumalpranay619@gmail.com>
2016-10-01 10:07:27 +0530

    added program for add even

---
+++ OddEven.py
@@ -0,0 +1,6 @@
+print('Program to identify if the number is even or odd')
+number=int(input('Enter number'))
+if number % 2== 0 :
+    print('The number is even')
+else :
+    print('The number is odd')
